9abe9c7a6469bf4b927af9d50f8ce14d244fcff9
Aareefa <aareefashaik00@gmail.com>
2026-04-15 15:36:59 +0530

    done my first day as the python

---
+++ day1.py
@@ -0,0 +1,37 @@
+'''print("This is my first day at codegnan")
+what is python?
+--> Python is a high-level-language means no need to worry about llow-level management like memory allocation.
+'''
+'''
+-->Python never save anything perminantly, it will only save at run time
+'''
+a = 0
+b = 7
+print(id(a))
+
+'''
+--> Python supports object oriented language
+'''
+a = int(input(" enter a num: " ))
+b = int(input(" enter a num: " ))
+print(a+b)
+print(f"b = {b}")
+print(f"a = {a}")
+
+'''
+-->This is also called dynamically-typed language no need to mention the type of data your take in a variable...
+'''
+a = 9
+b = "aareefa"
+print(type(a))
+print(type(b))
+
+'''
+-->Python is called interpered language means the python code will executes line by line 
+'''
+a =5
+b =5
+print(a+b)
+'''
+-->syntax less language
+'''
