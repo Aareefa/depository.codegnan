7694bf6846ffe4faf6ccfcc5ab1fd0d72648051a
Aareefa <aareefashaik00@gmail.com>
2026-06-27 16:48:14 +0530

    Data Analysis

---
+++ day30.py
@@ -0,0 +1,60 @@
+'''
+Data Analysis
+-------------
+->DA is the processof inspecting,cleaning,transforming,modeling data to discover
+useful insights, support decision_making..
+
+1.Descriptive Analysis
+----------------------
+Summarizing Data
+
+2.Diagnostic Analysis
+---------------------
+Understanding causes such as sales dropped
+
+3.Preditive Analysis
+--------------------
+Forecasting future outcomes
+
+4.Prescriptive Analysis
+-----------------------
+Suggesting actions based on data (Marketing)
+
+
+NUmpy (ndrray)
+--------------
+->Numerical python(Numpy)
+->It provides support for multi_dimensional arrays
+
+Creating Numpy arrays
+---------------------
+1D array
+import numpy as np
+arr_1 = np.array([1,2,3,4,5])
+print(arr_1)
+
+2D array
+import numpy as np
+arr_1 = np.array([[1,2,3,4],[6,7,8,9]])
+print(arr_1)
+
+multi array
+import numpy as np
+arr_1 = np.array([[1,2],[3,4],[6,7],[8,9]])
+print(arr_1)
+
+arange(start,end,step)
+'''
+import numpy as np
+arr_1 = np.arange(1,20,2)
+print(arr_1)
+
+
+
+
+
+
+
+
+
+
